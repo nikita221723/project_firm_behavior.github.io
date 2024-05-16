2a5d2e5b9bd21d860d69e509a18d2ff497c5b86f
Nikita Bakhishev <nikita.bakhishev@gmail.com>
2024-05-17 02:34:35 +0300

    almost full pc, some monopoly

--- application2.js
+++ application2.js
@@ -34,21 +34,27 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             name: 'max Revenue - costs',
         }
+        const trace4 = {
+            x: [],
+            y: [],
+            type: 'scatter',
+            name: 'Monopoly profit',
+        }
 
         if (p_equilibrium) {
             trace3.x.push(q_equilibrium, q_equilibrium);
-            trace3.y.push(Math.max(0, a1 * q_equilibrium * q_equilibrium + b1 * q_equilibrium + c1), p_equilibrium * q_equilibrium);
+            trace3.y.push(0, p_equilibrium * q_equilibrium);
         }
       
         for (let x = 0; x <= 30; x += 0.1) {
           trace1.x.push(x);
           trace1.y.push(Math.max(0, a1 * x * x + b1 * x + c1)); 
-          if (p_equilibrium) {
-            trace2.x.push(x);
-            trace2.y.push(p_equilibrium * x); 
-          }
+          trace2.x.push(x)
+          trace2.y.push((a - b * x) * x)
+          trace4.x.push(x);
+          trace4.y.push((a - b * x) * x - (a1 * x * x + b1 * x + c1))
         }
-        const data = [trace1, trace2, trace3]
+        const data = [trace1, trace2, trace3, trace4]
         const layout = {
           showlegend: true,
           legend: {
@@ -58,12 +64,12 @@ document.addEventListener('DOMContentLoaded', () => {
           },
           title: 'Costs and Revenue',
           xaxis: { title: 'quantity'},
-          yaxis: { title: 'price', rangemode: "tozero", range: [0, 150]},
+          yaxis: { title: 'price', rangemode: "tozero", range: [-30, 150]},
           autosize: false,
             width: 450,
             height: 450,
         };
-        Plotly.newPlot('plot', data, layout, {displayModeBar: false});
+        Plotly.newPlot('plot_m', data, layout, {displayModeBar: false});
         eq_price(); // равновесную цену вызываем 
         eq_quantity(); // равновесное количество вызываем 
         producer_surplus(); // излишки
@@ -129,10 +135,10 @@ document.addEventListener('DOMContentLoaded', () => {
     let c1 = parseFloat(document.getElementById('c1-input').value);
     function DemandSupplyFunction() {
         let q_eq = 0
-        if (2 * a1 + b != 0) {
-            q_eq = (a - b1) / (2 * a1 + b)
+        if (2 * a1 + 2 * b != 0) {
+            q_eq = (a - b1) / (2 * a1 + 2 * b)
         }
-        if (q_eq > 0 && a - b * q_eq > 0) {
+        if (q_eq > 0 && a - b * q_eq > 0.01 && a - 2 * b * q_eq > 0.01) {
             p_equilibrium = a - b * q_eq;
             q_equilibrium = q_eq;
         } else {
@@ -152,7 +158,7 @@ document.addEventListener('DOMContentLoaded', () => {
             x: [],
             y: [],
             type: 'scatter',
-            name: 'Supply',
+            name: 'MC',
           };
         const trace3 = {
             x: [],
@@ -162,18 +168,42 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Equilibtium',
             marker: { size: 8 }
         }
+        const trace4 = {
+            x: [],
+            y: [],
+            type: 'scatter',
+            name: 'Marginal Revenue',
+        }
+        const trace5 = {
+            x: [],
+            y: [],
+            mode: 'lines',
+            line: {
+                dash: 'dot',
+                width: 4,
+                color: 'green',
+            },
+            showlegend: false
+        }
+        if (p_equilibrium) {
+            trace5.x.push(0, q_equilibrium, q_equilibrium)
+            trace5.y.push(p_equilibrium, p_equilibrium, 0)
+        }
+        
         for (let x = 0; x <= 30; x += 0.1) {
           trace1.x.push(x);
           trace1.y.push(Math.max(a - b * x, 0));
           trace2.x.push(x);
-          trace2.y.push(Math.max(0, 2 * a1 * x + b1));  
+          trace2.y.push(Math.max(0, 2 * a1 * x + b1));
+          trace4.x.push(x);
+          trace4.y.push(Math.max(0, a - 2 * b * x));  
           if (p_equilibrium) {
             trace3.x.push(q_equilibrium);
             trace3.y.push(p_equilibrium);
           }
         }
       
-        const data = [trace1, trace2, trace3];
+        const data = [trace1, trace2, trace3, trace4, trace5];
         const layout = {
            showlegend: true,
            legend: {
@@ -183,13 +213,13 @@ document.addEventListener('DOMContentLoaded', () => {
             },
           title: 'Demand and Supply',
           xaxis: { title: 'quantity' },
-          yaxis: { title: 'price', rangemode: 'tozero', range: [0, 30]},
+          yaxis: { title: 'price', rangemode: 'tozero', range: [0, 30] },
           autosize: false,
             width: 450,
             height: 450,
         };
       
-        Plotly.newPlot('plot1', data, layout, {displayModeBar: false});
+        Plotly.newPlot('plot1_m', data, layout, {displayModeBar: false});
         eq_price(); // равновесную цену вызываем 
         eq_quantity(); // равновесное количество вызываем 
         producer_surplus(); // излишки
@@ -254,64 +284,61 @@ document.addEventListener('DOMContentLoaded', () => {
     let a1 = parseFloat(document.getElementById('a1-input').value);
     let b1 = parseFloat(document.getElementById('b1-input').value);
     let c1 = parseFloat(document.getElementById('c1-input').value);
+    let a = parseFloat(document.getElementById('a-input').value);
+    let b = parseFloat(document.getElementById('b-input').value);
     function CostsFunction() {
+
         const trace1 = {
-          x: [],
-          y: [],
-          type: 'scatter',
-          name: 'ATC (average total costs)',
+            x: [],
+            y: [],
+            type: 'scatter',
+            name: 'MR (Marginal Revenue)',
+            line: {
+                color: 'red'
+            }
         };
 
         const trace2 = {
             x: [],
             y: [],
             type: 'scatter',
-            name: 'AVC (average variable costs)',
-        };
-
-        const trace3 = {
-            x: [],
-            y: [],
-            type: 'scatter',
             name: 'MC (marginal costs)',
         };
 
-        const trace4 = {
-            x: [],
-            y: [],
-            type: 'scatter',
-            name: 'equilibrium price',
-        }
-        const trace5 = {
-            x: [],
-            y: [],
-            mode: 'lines',
-            line: {
-                dash: 'dot',
-                width: 4
-            },
-            showlegend: false
-        }
+        // const trace4 = {
+        //     x: [],
+        //     y: [],
+        //     type: 'scatter',
+        //     name: 'equilibrium price',
+        // }
+        // const trace5 = {
+        //     x: [],
+        //     y: [],
+        //     mode: 'lines',
+        //     line: {
+        //         dash: 'dot',
+        //         width: 4
+        //     },
+        //     showlegend: false
+        // }
 
-        if (p_equilibrium) {
-            trace5.x.push(q_equilibrium, q_equilibrium);
-            trace5.y.push(0, 30);
-        }
+        // if (p_equilibrium) {
+        //     trace5.x.push(q_equilibrium, q_equilibrium);
+        //     trace5.y.push(0, 30);
+        // }
       
         for (let x = 0; x <= 30; x += 0.1) {
           trace1.x.push(x);
-          trace1.y.push(Math.max(a1 * x + b1 + c1 / x, 0)); 
+          trace1.y.push(Math.max(a - 2 * b * x, 0)); 
           trace2.x.push(x);
-          trace2.y.push(Math.max(a1 * x + b1, 0)); 
-          trace3.x.push(x);
-          trace3.y.push(Math.max(2 * a1 * x + b1, 0)); 
-          if (p_equilibrium) {
-            trace4.x.push(x);
-            trace4.y.push(p_equilibrium);
-          }
+          trace2.y.push(Math.max(2 * a1 * x + b1, 0)); 
+        //   if (p_equilibrium) {
+        //     trace4.x.push(x);
+        //     trace4.y.push(p_equilibrium);
+        //   }
         }
       
-        const data = [trace1, trace2, trace3, trace4, trace5];
+        const data = [trace1, trace2];
         const layout = {
            showlegend: true,
            legend: {
@@ -319,7 +346,7 @@ document.addEventListener('DOMContentLoaded', () => {
               xanchor: 'right',
               y: 1
             },
-          title: 'ATC, AVC, MC',
+          title: 'Demand, MR, MC',
           xaxis: { title: 'quantity' },
           yaxis: { title: 'price', rangemode: 'tozero', range: [0, 30] },
           autosize: false,
@@ -327,7 +354,7 @@ document.addEventListener('DOMContentLoaded', () => {
             height: 450, 
         };
       
-        Plotly.newPlot('plot2', data, layout, {displayModeBar: false});
+        Plotly.newPlot('plot2_m', data, layout, {displayModeBar: false});
         eq_price(); // равновесную цену вызываем 
         eq_quantity(); // равновесное количество вызываем 
         producer_surplus(); // излишки
@@ -338,6 +365,8 @@ document.addEventListener('DOMContentLoaded', () => {
         a1 = parseFloat(document.getElementById('a1-input').value);
         b1 = parseFloat(document.getElementById('b1-input').value);
         c1 = parseFloat(document.getElementById('c1-input').value);
+        a = parseFloat(document.getElementById('a-input').value);
+        b = parseFloat(document.getElementById('b-input').value);
         CostsFunction();
     }
     document.getElementById('a-input').addEventListener('input', (e) => {
